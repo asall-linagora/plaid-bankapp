660f53eca1cf4c361f18aba163007994ba60ee5f
Alpha SALL <alsall@free.fr>
2017-02-19 17:45:13 -0500

    app scafolding with yeaman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPlaidAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularPlaidAppApp
+ */
+angular.module('angularPlaidAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPlaidAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularPlaidAppApp
+ */
+angular.module('angularPlaidAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
